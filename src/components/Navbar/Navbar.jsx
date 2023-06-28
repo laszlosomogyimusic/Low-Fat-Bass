5aa361e60ddd10fdfbc03f8aa968cfbe1ef69b28
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-28 09:06:21 +0100

    Navbar finished

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { GiHamburgerMenu, GiMusicSpell } from 'react-icons/gi';
+
+
+import images from '../../constants/images';
+import './Navbar.css'
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="navbar">
+      <figure className="navbar__figure">
+        <img className="navbar__image" src={images.laszloSomogyiMusic} alt="app logo" />
+      </figure>
+      <ul className="navbar__links">
+        <li className="p--ubuntu navbar__linkitem"><a href="#home">Home</a></li>
+        <li className="p--ubuntu navbar__linkitem"><a href="#about">About</a></li>
+        <li className="p--ubuntu navbar__linkitem"><a href="#menu">Menu</a></li>
+        <li className="p--ubuntu navbar__linkitem"><a href="#awards">Awards</a></li>
+        <li className="p--ubuntu navbar__linkitem"><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="navbar__login">
+        <a href="#login" className="p--ubuntu navbar__anchor">Log In / Register</a>
+        <div className="navbar__decoration" />
+        <a href="/" className="p--ubuntu navbar__anchor">Book Table</a>
+      </div>      
+      <div className="navbar__hamburgermenu">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        
+        {toggleMenu && (
+          <div className="navbar__hamburgermenu-content flex--center slide-bottom">
+            <GiMusicSpell fontSize={27} className="navbar__hamburgermenu-closebutton" onClick={() => setToggleMenu(false)} />
+            <ul className="navbar__hamburgermenu-links">
+              <li className="p--ubuntu navbar__hamburgermenu-linkitem"><a href="#home">Home</a></li>
+              <li className="p--ubuntu navbar__hamburgermenu-linkitem"><a href="#about">About</a></li>
+              <li className="p--ubuntu navbar__hamburgermenu-linkitem"><a href="#menu">Menu</a></li>
+              <li className="p--ubuntu navbar__hamburgermenu-linkitem"><a href="#awards">Awards</a></li>
+              <li className="p--ubuntu navbar__hamburgermenu-linkitem"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>   
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import logoCircle from '../assets/21_InCircle.png';
+import logoNoCircle from '../assets/21_NoCircle.png';
+import laszloSomogyiMusic from '../assets/LaszloSomogyiMusic.png';
+
+export default {
+  logoCircle,
+  logoNoCircle,
+  laszloSomogyiMusic,
+};
