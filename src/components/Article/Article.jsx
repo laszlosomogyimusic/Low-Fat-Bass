b889041f8198a5a8080c74d5166aca5303b786b6
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-07-02 09:54:16 +0100

    working on article text modifier need to check that, still not working

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,6 +3,34 @@ import "./Article.css"
 
 import  { images } from "../../constants/"
 
+let articleClassName = "article";
+let titleClassName = "article__title";
+let subtitleClassName = "article__subtitle";
+let textClassName = "article__text";
+
+const initClassNames = (titleModifierClass, subtitleModifierClass, textModifierClass, justifyContentTo) => {
+  console.log("initClassNames: " + textClassName);
+  switch(justifyContentTo) {
+    case "left":
+      articleClassName += " article--justifyleft"   //make sure there is a leading space in the string
+    break;
+    case "right":
+      articleClassName += " article--justifyright"  //make sure there is a leading space in the string
+    break;
+    case "center":
+      articleClassName += " article--justifycenter" //make sure there is a leading space in the string
+    break;
+    default:
+      articleClassName += " article--justifyleft"   //make sure there is a leading space in the string
+    break;
+  }
+
+  titleClassName += titleModifierClass ? ` ${titleModifierClass}` : "";           //make sure there is a leading space in the string
+  subtitleClassName += subtitleModifierClass ? ` ${subtitleModifierClass}` : "";  //make sure there is a leading space in the string
+  textClassName += textModifierClass ? ` ${textModifierClass}` : "";              //make sure there is a leading space in the string
+
+}
+
 const HorizontalDrumstick = ({ doRotateImage = false }) => {
   return (
     <img 
@@ -13,37 +41,24 @@ const HorizontalDrumstick = ({ doRotateImage = false }) => {
   )
 }
 
-const Article = ({title, text = "", isTextColorGrey=false, subtitle = "", justifyContentTo = "", buttonText = "" }) => {
-  let articleClassName = "";
-  switch(justifyContentTo) {
-    case "left":
-      articleClassName = "article article--justifyleft"
-    break;
-    case "right":
-      articleClassName = "article article--justifyright"
-    break;
-    case "center":
-      articleClassName = "article article--justifycenter"
-    break;
-    default:
-      articleClassName = "article article--justifyleft"
-    break;
-  }
-  
+const Article = ({title = "", titleModifierClass = "", text = "", textModifierClass = "", isTextColorGrey=false, subtitle = "", subtitleModifierClass= "", justifyContentTo = "", buttonText = "" }) => {
+  initClassNames(titleModifierClass, subtitleModifierClass, textModifierClass, justifyContentTo);
+  console.log("Article: " + textClassName);
+
   return (
     <article className={articleClassName}>
       { subtitle ? (
           <div>
-            <p className="article__subtitle">{subtitle}</p>
+            <p className={subtitleClassName}>{subtitle}</p>
             <HorizontalDrumstick
               doRotateImage = {justifyContentTo === "left" ? false : true} 
             />
-            <h1 className="article__title">{title}</h1>
+            <h1 className={titleClassName}>{title}</h1>
           </div>
           )
         : (
             <div>
-              <h1 className="article__title">{title}</h1>
+              <h1 className={titleClassName}>{title}</h1>
               <HorizontalDrumstick
                 doRotateImage = {justifyContentTo === "left" ? false : true} 
               />
@@ -52,7 +67,7 @@ const Article = ({title, text = "", isTextColorGrey=false, subtitle = "", justif
       }
       { text && (
         <p 
-          className={`article__text ${isTextColorGrey ? "article__text--grey" : ""}`}>{text}</p>
+          className={textClassName}>{text}</p>
       )}
 
       { buttonText && (
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -7,14 +7,15 @@ import { Article } from '../../components';
 
 const AboutUs = () => (
   <section className="aboutus app__bg app__flex--center app__section" id="about">
-    <div className="aboutus__overlay app__flex--center">
-      <img src={images.logoNoCircle} alt="logo 21"/>
+    <div className="aboutus__logo21 app__flex--center">
+      <img className="aboutus__logo21--image" src={images.logoNoCircle} alt="logo 21"/>
     </div>
     <section className="aboutus__content app__flex--center">
       <Article 
         justifyContentTo = "right"
         title = {aboutUs.title}
         text = {aboutUs.text}
+        textModifierClass="aboutus__article-text--grey"
         isTextColorGrey = {true}
         buttonText = "Know more" />
       <figure className="aboutus__drumsticks flex__center">
