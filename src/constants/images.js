3eab5887c9f676e3cf74effb0e8c2b698fe0711c
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-07-09 19:46:33 +0100

    Owner finished

--- src/constants/images.js
+++ src/constants/images.js
@@ -6,6 +6,7 @@ import guitar from '../assets/guitar.svg';
 import drumsticksVertical from '../assets/drumsticks_vertical.png';
 import drumstickHorizontal from '../assets/drumstick_horizontal.png';
 import bassGuitar from '../assets/bassguitar.jpg'
+import owner from '../assets/owner.png'
 
 export default {
   logoCircle,
@@ -16,4 +17,5 @@ export default {
   drumsticksVertical,
   drumstickHorizontal,
   bassGuitar,
+  owner
 };
--- src/container/Owner/Owner.jsx
+++ src/container/Owner/Owner.jsx
@@ -9,13 +9,13 @@ const Owner = () => {
     type: "normal",
     justifyContentTo: "left",
     title:"What we believe in",
-    subtitle: "Owner's Word", 
+    subtitle: "The Owner's Words", 
     text: "Do the music. Do the dance.",
   }  
   return (
     <section className="owner app__bg app__wrapper app__section" id="home">
       <figure className="app__wrapper-figure">
-        <img className="owner__image" src={images.welcome} alt="header img" />
+        <img className="owner__image" src={images.owner} alt="header img" />
       </figure>    
       <Article {...ownerArticle} />
     </section>
