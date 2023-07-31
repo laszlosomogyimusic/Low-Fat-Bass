5a343cad563cb7e0109062e03be059b7c7940747
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-07-31 16:18:25 +0200

    Footer is nearly done

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import { Article } from '../../components';
+
 import './Newsletter.css';
 
-const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+const Newsletter = () => {
+  let newsletterArticle = {
+    type: "normal",
+    justifyContentTo: "center",
+    title: "Subscribe To Our Newsletter",
+    subtitle: "Newsletter",
+    text: "And never miss latest Updates!"
+  }
+
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <Article {...newsletterArticle} />
+      </div>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Enter your email address" />
+        <button type="button" className="article__button">Subscribe</button>
+      </div>
+    </div>
+  )
+};
 
 export default Newsletter;
