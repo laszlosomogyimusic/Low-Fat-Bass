c78972392ebe1340470d3bba1f97bf602fcc0c73
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-07-31 14:44:55 +0200

    Gallery finished

--- src/constants/images.js
+++ src/constants/images.js
@@ -13,6 +13,12 @@ import award02 from '../assets/award02.png'
 import award03 from '../assets/award03.png'
 import award05 from '../assets/award05.png'
 
+import gallery01 from '../assets/gallery01.png'
+import gallery02 from '../assets/gallery02.png'
+import gallery03 from '../assets/gallery03.png'
+import gallery04 from '../assets/gallery04.png'
+
+
 export default {
   logoCircle,
   logoNoCircle,
@@ -28,4 +34,8 @@ export default {
   award02, 
   award03,
   award05,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
 };
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 
+import { images } from "../../constants"
+import { Article } from '../../components';
+
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+  const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+
+  let galleryArticle = {
+    type: "normal",
+    justifyContentTo: "left",
+    title:"Photo Gallery",
+    subtitle: "Instagram", 
+    text: "Please see few stunning pictures from our instagram",
+    buttonText: "View more"
+  }  
+  return (
+    <section className="gallery app__flex--center" id="gallery">
+      <div className="gallery__content">
+        <Article {...galleryArticle} />
+      </div>
+
+      <div className="gallery__images">
+        <div className="gallery__images-container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div className="gallery__images-card app__flex--center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+
+
+    </section>
+  );
+}
 
 export default Gallery;
